b211acecaf95e564f7088f4094fde573c9b44076
Iulia Pop <andreeapopmonica@gmail.com>
2024-01-10 00:40:19 +0200

    first try

---
+++ laburi_maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace laburi_maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
